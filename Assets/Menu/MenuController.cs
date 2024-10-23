9a6ce062655c764a4ba6f981c7050a70e3a5958b
rogeriocabral30 <rogeriocabral13@gmail.com>
2024-10-23 14:27:34 -0300

    23/10/24
    botao new game funcionando, alteraçao nas cores dos prefabs da game play

--- Assets/Menu/MenuController.cs
+++ Assets/Menu/MenuController.cs
@@ -2,21 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Video;
+using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour
 {
     public VideoPlayer videoPlayer;
-    public GameObject menuOpcoes, rawImage; 
+    public GameObject menuOpcoes, rawImage;
     public AudioSource selectSound;
     private Animator animatorRawImage;
+    public GameObject painelOpcoes;
 
-    
     void Start()
     {
         // Verificação se rawImage não é nulo antes de ativar
         if (rawImage != null)
         {
-            rawImage.SetActive(false); 
+            rawImage.SetActive(false);
             animatorRawImage = rawImage.GetComponent<Animator>();
         }
         else
@@ -24,35 +25,53 @@ public class MenuController : MonoBehaviour
             Debug.LogWarning("RawImage is not assigned!");
         }
 
-        // Verificação se selectSound não é nulo
+        // Verificação se selectSound 
         if (selectSound == null)
         {
             Debug.LogWarning("Select Sound is not assigned!");
         }
+
+        // Inicialmente, painel de opções está desativado
+        painelOpcoes.SetActive(false);
     }
 
-    
     void Update()
     {
         // Verifica se o vídeo não está tocando e se alguma tecla foi pressionada
         if (videoPlayer != null && !videoPlayer.isPlaying && Input.anyKeyDown)
         {
-            // Toca o som de seleção, se não for nulo
+            // Toca o som de seleção
             if (selectSound != null)
             {
                 selectSound.Play();
             }
-
-            
             videoPlayer.Play();
-
             // Ativa a animação e os objetos do menu
             if (animatorRawImage != null)
             {
                 animatorRawImage.SetTrigger("fadeIn");
             }
-            rawImage.SetActive(true); 
-            menuOpcoes.SetActive(true); 
+            rawImage.SetActive(true);
+            menuOpcoes.SetActive(true);
         }
     }
+
+    public void AbrirOpcoes()
+    {
+        // Ativa o painel de opções
+        painelOpcoes.SetActive(true);
+    }
+
+    public void FecharOpcoes()
+    {
+        // Desativa o painel de opções
+        painelOpcoes.SetActive(false);
+    }
+
+    public void IniciarJogo()
+    {
+        // Carrega a gameplay
+        SceneManager.LoadScene("SampleScene");
+    }
 }
+
